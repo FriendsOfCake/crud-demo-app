1ab91c45519ab2d1024aceaaa23273bdf37e03a1
bravo-kernel <bravo-kernel@users.noreply.github.com>
2015-07-24 17:27:22 +0200

    Adds function comments

--- App/Controller/PostsController.php
+++ App/Controller/PostsController.php
@@ -9,10 +9,22 @@ class PostsController extends AppController {
 
 	public $paginate = ['limit' => 5];
 
+	/**
+	 * beforeFilter.
+	 *
+	 * @param Cake\Event\Event $event Event instance
+	 * @return \Cake\Network\Response
+	 */
 	public function beforeFilter(Event $event) {
 		return parent::beforeFilter($event);
 	}
 
+	/**
+	 * Add action.
+	 *
+	 * @param void
+	 * @return \Cake\Network\Response
+	 */
 	public function index() {
 		$this->Crud->action()->config('scaffold.fields_blacklist', ['id']);
 		$this->Crud->action()->config('scaffold.fields', ['name', 'is_active', 'comment_count']);
@@ -24,6 +36,12 @@ class PostsController extends AppController {
 		return $this->Crud->execute();
 	}
 
+	/**
+	 * Add action.
+	 *
+	 * @param void
+	 * @return \Cake\Network\Response
+	 */
 	public function add() {
 		return (new \Crud\Action\AddAction($this))->handle();
 	}
